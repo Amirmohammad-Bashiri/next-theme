3950b75a470b639f8cf141aea09248a78e0a1ecc
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2024-02-09 20:44:05 +0330

    Initial commit

---
+++ src/themes.ts
@@ -0,0 +1,6 @@
+export const themes = {
+  primary: "var(--color-primary)",
+  secondary: "var(--color-secondary)",
+  buttons: "var(--color-buttons)",
+  typography: "var(--color-typography)",
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { themes } from "./src/themes";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,11 +9,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: themes,
     },
   },
   plugins: [],
